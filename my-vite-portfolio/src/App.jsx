c6135e569ef5cfba3783bf8c8c66a878ed896ed4
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-01-06 12:27:21 +0800

    Edited `react-router` configuration for home path

--- my-vite-portfolio/src/App.jsx
+++ my-vite-portfolio/src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 export default function App() {
   return <div>
       <Routes>
-        <Route path="*" element={<Home/>}/>
+        <Route path="*/web-portfolio" element={<Home/>}/>
         <Route path="*/resume" element={<Resume/>}/>
       </Routes>
   </div>
